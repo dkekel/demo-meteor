0c12f5e54cfc7ef42a2d9b918dd26690840c6e6f
dkekel <dkekel@gmail.com>
2019-05-31 22:23:48 +0200

    Configure Mongo DB, fetch tasks from the DB

--- client/task/list.js
+++ client/task/list.js
@@ -1,7 +1,7 @@
+import { Tasks } from '../../api/tasks';
+
 Template.list.helpers({
-    tasks: [
-        { text: 'Set-up the environment' },
-        { text: 'Create a new project' },
-        { text: 'Add tasks list page '}
-    ]
+    tasks() {
+        return Tasks.find({});
+    }
 });
