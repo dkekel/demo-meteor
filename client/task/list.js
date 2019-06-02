d9d728ed82e5a35f3a362acf11c12b2b64b32034
dkekel <dkekel@gmail.com>
2019-06-02 17:58:29 +0200

    Add form to add new tasks to the list

--- client/task/list.js
+++ client/task/list.js
@@ -1,7 +1,24 @@
-import { Tasks } from '../../api/tasks';
+import {Tasks} from '../../api/tasks';
 
 Template.list.helpers({
     tasks() {
-        return Tasks.find({});
+        return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+});
+
+Template.list.events({
+    'submit #newTask'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+        // Insert a task into the collection
+        Tasks.insert({
+            text,
+            createdAt: new Date(), // current time
+        });
+        // Clear form
+        target.text.value = '';
     }
 });
